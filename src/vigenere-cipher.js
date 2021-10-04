1d3b80eb9e1e0baa3216424e453f4f9c6c3fb76e
Ilya Dzyuin <ilua.dzuin@gmail.com>
2021-10-05 03:53:31 +0400

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,10 +20,33 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-	encrypt() {
-
+	constructor(dir = true) {
+		this.dir = dir;
+		this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 	}
-	decrypt() {
 
+	crypt(str, key, method) {
+		if (!str || !key) throw Error('Incorrect arguments!');
+		str = str.toUpperCase().split('');
+		while (key.length < str.length) key += key;
+		key = key.toUpperCase().split('');
+		let shift;
+		let l = this.alphabet.length;
+		for (let i = 0, j = 0; i < str.length; i++) {
+			if (this.alphabet.includes(str[i])) {
+				shift = (method === 'encrypt')
+					? (this.alphabet.indexOf(str[i]) + this.alphabet.indexOf(key[j])) % l
+					: (method === 'decrypt')
+						? (this.alphabet.indexOf(str[i]) - this.alphabet.indexOf(key[j]) + l) % l
+						: 0
+				j++;
+				str[i] = this.alphabet[shift];
+			}
+		}
+		if (this.dir) return str.join('')
+		else return str.reverse().join('')
 	}
+
+	encrypt = (str, key) => this.crypt(str, key, 'encrypt');
+	decrypt = (str, key) => this.crypt(str, key, 'decrypt');
 }
